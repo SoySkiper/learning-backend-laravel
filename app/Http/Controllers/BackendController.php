d493a5b3857afaa178e33c3e9eac83fd6f54140e
Cristian Primero <soyskiper@gmail.com>
2025-07-01 13:22:57 -0600

    HTTP requests using POST method

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
 
 class BackendController extends Controller
 {
@@ -24,5 +25,16 @@ class BackendController extends Controller
         } else {
             return response()->json(["error" => "Person not found"], Response::HTTP_NOT_FOUND);
         }
+    }
+
+    public function create(Request $request){
+        $person = [
+            "id" => count($this->names) + 1,
+            "name" => $request->input('name'),
+            "age" => $request->input('age')
+        ];
+
+        $this->names[$person['id']] = $person;
+        return response()->json(["message" => "Person created successfully", "person" => $person], Response::HTTP_CREATED);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,5 @@ Route::get('/test', function(){
 Route::get('/backend', [BackendController::class, 'getAll']);
 
 Route::get('/backend/{id?}', [BackendController::class, 'get']);
+
+Route::post('/backend', [BackendController::class, 'create']);
