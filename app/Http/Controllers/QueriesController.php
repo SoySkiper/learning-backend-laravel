25ad2dd84349bbdb0be3b44e7554613f30eda82f
Cristian Primero <soyskiper@gmail.com>
2025-07-17 17:16:38 -0600

    LIKE operator in Eloquent

--- app/Http/Controllers/QueriesController.php
+++ app/Http/Controllers/QueriesController.php
@@ -8,12 +8,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class QueriesController extends Controller
 {
-    public function getAll() {
+    public function getAll()
+    {
         $products = Product::all();
         return response()->json($products);
     }
 
-    public function getById(int $id) {
+    public function getById(int $id)
+    {
         $product = Product::find($id);
         if ($product) {
             return response()->json($product);
@@ -22,18 +24,28 @@ class QueriesController extends Controller
         }
     }
 
-    public function getNames() {
+    public function getNames()
+    {
         $products = Product::select('name')
             ->orderBy('name', 'desc')
             ->get();
         return response()->json($products);
     }
 
-    public function searchNames(string $name, float $price) {
+    public function searchNames(string $name, float $price)
+    {
         $products = Product::where('name', $name)
             ->where('price', '>', $price)
             ->orderBy('name', 'asc')
             ->select('name', 'description', 'price')
+            ->get();
+
+        return response()->json($products);
+    }
+
+    public function searchString(string $value)
+    {
+        $products = Product::where('description', 'like', "%{$value}%")
             ->get();
 
         return response()->json($products);
--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,5 @@ Route::get('/query', [QueriesController::class, 'getAll']);
 Route::get('/query/{id}', [QueriesController::class, 'getById']);
 Route::get('/query/method/names', [QueriesController::class, 'getNames']);
 Route::get('/query/method/search/{name}/{price}', [QueriesController::class, 'searchNames']);
+Route::get('/query/method/searchString/{value}', [QueriesController::class, 'searchString']);
 
